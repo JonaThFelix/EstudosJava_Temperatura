0fe07f80e63fa72ecbc1bf5c4418ed4bc5e35374
Jonathan Felix <123984244+JonaThFelix@users.noreply.github.com>
2023-08-17 21:17:36 -0300

    Update Temperatura.java

--- Temperatura.java
+++ Temperatura.java
@@ -7,9 +7,19 @@ public class Temperatura {
 		double f = 86; // variável double (float)
 		final int TrintaeDois = 32; // constante armazenando valor fixo, inteiro
 		final double Divisao = 5/9.0; // constante armazenando valor flutuante, float
-		
+
+		// First
 		double Celsius = (f - TrintaeDois) * Divisao; // var recebendo formulação acima
-		
 		System.out.println("A temperatura de F para C eh: "+ Celsius + " C");
+		
+		// Second
+		f = 120;
+		Celsius = (f - TrintaeDois) * Divisao;
+		System.out.println("Nova conversao : " + Celsius+ " C");
+
+		//Third
+		f = 250;
+		Celsius = (f - TrintaeDois) * Divisao;
+		System.out.println("Terceira e última conversao: " + Celsius + "C");
 	}
 }
